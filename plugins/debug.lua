02adb380de5060de90302bbd58683b27111878fe
siktirmirza <alicowman79@gmail.com>
2016-03-05 17:07:39 +0330

    Added files via upload

--- plugins/debug.lua
+++ plugins/debug.lua
@@ -1,206 +1,73 @@
-local ltn12 = require "ltn12"
---local http = require "socket.http"
-local https = require "ssl.https"
+local function get_msg_callback(extra, success, result)
+  --vardump(extra)
+  --vardump(success)
+  vardump(result)
+  local get_cmd = extra.get_cmd
+  local receiver = extra.receiver
+end
 
-local bot_key = "154113076:AAGqUrkMvVAOTX7j4c0PfQbDII-QPkH323I"
-local BASE_URL = 'https://api.telegram.org/bot'..bot_key.. '/'
-
-local function kick_user(user_id, chat_id)
-  local chat = 'chat#id'..chat_id
-  local user = 'user#id'..user_id
-	if user_id == our_id then
-		send_large_msg(chat,'Are you kidding?')
-		return nil
-	end
-	local data = load_data(_config.moderation.data)
-	if data[tostring('admins')] then
-		 if data[tostring('admins')][tostring(user_id)] then
-				send_large_msg(chat, 'You can\'t kick admin!')
-				return nil
-     end
+local function resuser(extra, success, result)
+  local receiver = extra.receiver
+  local username = extra.username
+  if success == 1 then
+  	vardump(result)
+  	send_large_msg(receiver, result.peer_id)
+  else
+  	send_large_msg(receiver, "User not found!")
   end
-  chat_del_user(chat, user, ok_cb, true)
 end
 
-local function ban_user(user_id, chat_id)
-  local hash =  'banned:'..chat_id..':'..user_id
-  redis:set(hash, true)
-	if user_id == our_id then
-		send_large_msg(chat, 'Are you kidding?')
-		return nil
-	end
-	local data = load_data(_config.moderation.data)
-	if data[tostring('admins')] then
-		 if data[tostring('admins')][tostring(user_id)] then
-				send_large_msg(chat, 'You can\'t ban admin!')
-				return nil
-     end
+local function generate_link(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  if success == 0 then
+    return send_large_msg(receiver, "Can't generate invite link for this group")
   end
-  kick_user(user_id, chat_id)
+  vardump(result)
+	return send_large_msg(receiver,'=> '..result)
 end
 
-local function superban_user(user_id, chat_id)
-  local hash =  'superbanned:'..user_id
-  redis:set(hash, true)
-	if user_id == our_id then
-		send_large_msg(chat, 'Are you kidding?')
-		return nil
-	end
-  kick_user(user_id, chat_id)
-end
-
-function is_botmod(botmsg)
-  local var = false
-  local data = load_data(_config.moderation.data)
-  local user = botmsg.from.id
-  if data[tostring(-botmsg.chat.id)] then
-    if data[tostring(-botmsg.chat.id)]['moderators'] then
-      if data[tostring(-botmsg.chat.id)]['moderators'][tostring(user)] then
-        var = true
-      end
-    end
-  end
-  if data['admins'] then
-    if data['admins'][tostring(user)] then
-      var = true
-    end
-  end
-  for v,user in pairs(_config.sudo_users) do
-    if user == botmsg.from.id then
-        var = true
-    end
-  end
-  return var
-end
-
-local function promote(user_id, user_name, chat_id)
-  local receiver = 'chat#id'..chat_id
-  local data = load_data(_config.moderation.data)
-	if not data[tostring(chat_id)] then
-		send_large_msg(receiver, 'Group is not added.')
-		return
-	end
-	if data[tostring(chat_id)]['moderators'][tostring(user_id)] then
-		send_large_msg(receiver, user_name..' is already a moderator.')
-		return
-  end
-    data[tostring(chat_id)]['moderators'][tostring(user_id)] = user_name
-    save_data(_config.moderation.data, data)
-    send_large_msg(receiver, '@'..user_name..' has been promoted.')
-		return
-end
-
-local function demote(user_id, user_name, chat_id)
-  local data = load_data(_config.moderation.data)
-  local receiver = 'chat#id'..chat_id
-	if not data[tostring(chat_id)] then
-		send_large_msg(receiver, 'Group is not added.')
-		return
-	end
-	if not data[tostring(chat_id)]['moderators'][tostring(user_id)] then
-		send_large_msg(receiver, user_name..' is not a moderator.')
-	end
-	data[tostring(chat_id)]['moderators'][tostring(user_id)] = nil
-	save_data(_config.moderation.data, data)
-	send_large_msg(receiver, '@'..user_name..' has been demoted.')
-	return
-end
-
-function on_bot_msg(botmsg)
-	if not botmsg.chat.type == "group" then return end
-	--vardump(botmsg)
-	if botmsg.reply_to_message then
-		if not is_botmod(botmsg) then return end
-		local chat_id = string.gsub(botmsg.reply_to_message.chat.id, "-", "")
-		local user_id = botmsg.reply_to_message.from.id
-		if botmsg.reply_to_message.from.username then
-			user_name = botmsg.reply_to_message.from.username
-		else
-			user_name = botmsg.reply_to_message.from.first_name
-		end
-		if botmsg.text == "!kick" then
-			kick_user(user_id, chat_id)
-		end
-		if botmsg.text == "!ban" then
-			ban_user(user_id, chat_id)
-		end
-		if botmsg.text == "!superban" then
-			superban_user(user_id, chat_id)
-		end
-		if botmsg.text == "!promote" then
-			promote(user_id, user_name, chat_id)
-		end
-		if botmsg.text == "!demote" then
-			demote(user_id, user_name, chat_id)
-		end
-	end
-end
-
-local function send_request(url)
-	local dat, res = https.request(url)
-	local tab = JSON.decode(dat)
-	if res ~= 200 then
-		print('Connection error.')
-		return false
-	end
-	if not tab.ok then
-		print(tab.description)
-		return false
-	end
-	return tab
-end
-
-function get_updates(offset)
-	local url = BASE_URL .. 'getUpdates?timeout=30'
-	if offset then
-		url = url .. '&offset=' .. offset
-	end
-	return send_request(url)
-end
-
-last_update = 0
 local function pre_process(msg)
-	local res = get_updates(last_update+1)
-	if not res then
-		print('Error getting updates.')
-	else
-		for i,v in ipairs(res.result) do
-			if v.update_id > last_update then
-				last_update = v.update_id
-				on_bot_msg(v.message, msg)
-			end
-		end
-	end
-	return msg
+  vardump(msg)
+  return msg
 end
 
 local function run(msg, matches)
-	if not is_chat_msg(msg) then return end
-	local res = get_updates(last_update+1)
-	if not res then
-		print('Error getting updates.')
-	else
-		for i,v in ipairs(res.result) do
-			if v.update_id > last_update then
-				last_update = v.update_id
-				on_bot_msg(v.message, msg)
-			end
-		end
-	end
+  local get_cmd = matches[1]
+  local receiver = get_receiver(msg)
+  if matches[1] == 'delete' and msg.reply_id then
+    delete_msg(msg.reply_id, ok_cb, false)
+  end
+  if matches[1] == 'resolve' then
+    local username = 'silenceangel'
+    resolve_username(username, resuser, {receiver=get_receiver(msg), username=username})
+  end
+  if matches[1] == 'link' then
+    if is_chat_msg(msg) then
+      export_chat_link(receiver, generate_link, {receiver=receiver})
+    end
+    if is_channel_msg(msg) then
+      export_channel_link(receiver, generate_link, {receiver=receiver})
+    end
+  end
+  if msg.reply_id then
+    print(msg.reply_id)
+    get_message(msg.reply_id, get_msg_callback, {get_cmd=get_cmd, receiver=receiver})
+  end
 end
 
 return {
-  description = "Simplest plugin ever!",
-  usage = "For debug purpose",
+  description = "Help plugin. Get info from other plugins.  ", 
+  usage = {
+    "!help: Show list of plugins.",
+    "!help all: Show all commands for every plugin.",
+    "!help [plugin name]: Commands for that plugin."
+  },
   patterns = {
-    "^(!kick)$",
-    "^(!ban)$",
-    "^(!superban)$",
-		"^(!promote)$",
-		"^(!demote)$"
+    "^[!/](debug)$",
+    "^[!/](delete)$",
+    "^[!/](resolve)$",
+    "^[!/](link)$",
   }, 
   run = run,
-  --pre_process = pre_process,
-  moderated = true,
-  hide = true
+  pre_process = pre_process
 }
--- plugins/help.lua
+++ plugins/help.lua
@@ -1,175 +1,112 @@
-do
- 
-function pairsByKeys(t, f)
-      local a = {}
-      for n in pairs(t) do table.insert(a, n) end
-      table.sort(a, f)
-      local i = 0      -- iterator variable
-      local iter = function ()   -- iterator function
-        i = i + 1
-        if a[i] == nil then return nil
-        else return a[i], t[a[i]]
-        end
-      end
-      return iter
-    end
- 
--- Returns true if is not empty
-local function has_usage_data(dict)
-  if (dict.usage == nil or dict.usage == '') then
-    return false
-  end
-  return true
-end
- 
--- Get commands for that plugin
-local function plugin_help(name,number,requester)
-  local plugin = ""
-  if number then
-    local i = 0
-    for name in pairsByKeys(plugins) do
-      if plugins[name].hidden then
-        name = nil
-      else
-        i = i + 1
-        if i == tonumber(number) then
-          plugin = plugins[name]
-        end
-      end
-    end
-  else
-    plugin = plugins[name]
-    if not plugin then return nil end
-  end
- 
-    local text = ""
-    if (type(plugin.usage) == "table") then
-      for ku,usage in pairs(plugin.usage) do
-          if ku == 'user' then -- usage for user
-              if (type(plugin.usage.user) == "table") then
-                  for k,v in pairs(plugin.usage.user) do
-                      text = text..v..'\n'
-                  end
-              elseif has_usage_data(plugin) then -- Is not empty
-                  text = text..plugin.usage.user..'\n'
-              end
-          elseif ku == 'moderator' then -- usage for moderator
-              if requester == 'moderator' or requester == 'admin' or requester == 'sudo' then
-                  if (type(plugin.usage.moderator) == "table") then
-                      for k,v in pairs(plugin.usage.moderator) do
-                          text = text..v..'\n'
-                      end
-                  elseif has_usage_data(plugin) then -- Is not empty
-                      text = text..plugin.usage.moderator..'\n'
-                  end
-              end
-          elseif ku == 'admin' then -- usage for admin
-              if requester == 'admin' or requester == 'sudo' then
-                  if (type(plugin.usage.admin) == "table") then
-                      for k,v in pairs(plugin.usage.admin) do
-                          text = text..v..'\n'
-                      end
-                  elseif has_usage_data(plugin) then -- Is not empty
-                      text = text..plugin.usage.admin..'\n'
-                  end
-              end
-          elseif ku == 'sudo' then -- usage for sudo
-              if requester == 'sudo' then
-                  if (type(plugin.usage.sudo) == "table") then
-                      for k,v in pairs(plugin.usage.sudo) do
-                          text = text..v..'\n'
-                      end
-                  elseif has_usage_data(plugin) then -- Is not empty
-                      text = text..plugin.usage.sudo..'\n'
-                  end
-              end
-          else
-              text = text..usage..'\n'
-          end
-      end
-      text = text..'======================\n'
-    elseif has_usage_data(plugin) then -- Is not empty
-      text = text..plugin.usage..'\n======================\n'
-    end
-    return text
-end
- 
- 
--- !help command
-local function telegram_help()
-  local i = 0
-  local text = "Plugins list:\n\n"
-  -- Plugins names
-  for name in pairsByKeys(plugins) do
-    if plugins[name].hidden then
-      name = nil
-    else
-    i = i + 1
-    text = text..i..'. '..name..'\n'
-    end
-  end
-  text = text..'\n'..'There are '..i..' plugins help available.'
-  text = text..'\n'..'Write "!help [plugin name]" or "!help [plugin number]" for more info.'
-  text = text..'\n'..'Or "!help all" to show all info.'
-  return text
-end
- 
- 
--- !help all command
-local function help_all(requester)
-  local ret = ""
-  for name in pairsByKeys(plugins) do
-    if plugins[name].hidden then
-      name = nil
-    else
-      ret = ret .. plugin_help(name, nil, requester)
-    end
-  end
-  return ret
-end
- 
 local function run(msg, matches)
-  if is_sudo(msg) then
-      requester = "sudo"
-  elseif is_admin(msg) then
-      requester = "admin"
-  elseif is_momod(msg) then
-      requester = "moderator"
-  else
-      requester = "user"
-  end
-  if matches[1] == "!help" then
-    return telegram_help()
-  elseif matches[1] == "!help all" then
-    return help_all(requester)
-  else
-    local text = ""
-    if tonumber(matches[1])  then
-      text = plugin_help(nil, matches[1], requester)
-    else
-      text = plugin_help(matches[1], nil, requester)
-    end
-    if not text then
-      text = telegram_help()
-    end
+  if is_chat_msg(msg) then
+    local text = [[‌✅Commands part #1
+💭/close|open link :- anti link
+💭/close|open member :- anti invite member
+💭/close|open name :- lock name
+💭/close|open image :- anti image
+💭/close|open photo :- lock photo
+💭/close|open sticker :- anti sticker
+💭/close|open file :- anti file
+💭/close|open chat :- close all Gp
+➕
+✅Commands for control member
+💭/kick : by <reply|id|username>
+💭/ban : by <reply|id|username>
+💭/unban : by <reply|id|username>
+💭/kickme
+💭/silent :- for one member by <id|reply|username>
+💭/unsilent :- cancel silent for member by <id|reply|username> 
+
+➕
+✅Group control 
+💭/rules
+💭/setrules <write rules>
+💭/about
+💭/setabout <write about> :- for Supergroups put Descripion
+💭/setphoto : then send photo
+💭/setname <write name>
+💭/id
+💭/ids chat :- show id and username all member in Gp
+💭/group settings :- settings Gp
+💭/getlink : send link in your pv
+💭/relink <id-chat> : change link group and send new link your pv
+💭/modlist :- show list moderation
+💭/help :- show the list help for you
+
+➕
+✅ Group Promote  commands
+💭/spromote : up leader by <reply|id|username> 
+💭/sdemote : in promote by <reply|id|username>
+💭/promote : by <reply|id|username> 
+💭/demote : by <reply|id|username> 
+
+➖🔸➖🔹➖🔸➖🔹➖
+Version :- 1.5 Telegram Bot 
+Developer :- " @Mustafaflux "
+]]
     return text
   end
+  if is_channel_msg(msg) then
+    local text = [[‌✅Commands part #1
+💭/close|open link :- anti link
+💭/close|open member :- anti invite member
+💭/close|open name :- lock name
+💭/close|open image :- anti image
+💭/close|open photo :- lock photo
+💭/close|open sticker :- anti sticker
+💭/close|open file :- anti file
+💭/close|open chat :- close all Gp
+➕
+✅Commands for control member
+💭/kick : by <reply|id|username>
+💭/ban : by <reply|id|username>
+💭/unban : by <reply|id|username>
+💭/kickme
+💭/silent :- for one member by <id|reply|username>
+💭/unsilent :- cancel silent for member by <id|reply|username> 
+
+➕
+✅Group control 
+💭/rules
+💭/setrules <write rules>
+💭/about
+💭/setabout <write about> :- for Supergroups put Descripion
+💭/setphoto : then send photo
+💭/setname <write name>
+💭/id
+💭/ids chat :- show id and username all member in Gp
+💭/group settings :- settings Gp
+💭/getlink : send link in your pv
+💭/relink <id-chat> : change link group and send new link your pv
+💭/modlist :- show list moderation
+💭/help :- show the list help for you
+
+➕
+✅ Group Promote  commands
+💭/spromote : up leader by <reply|id|username> 
+💭/sdemote : in promote by <reply|id|username>
+💭/promote : by <reply|id|username> 
+💭/demote : by <reply|id|username> 
+
+➖🔸➖🔹➖🔸➖🔹➖
+Version :- 1.5 Telegram Bot 
+Developer :- " @Mustafaflux "
+]]
+    return text
+  else
+    local text = [[aaa]]
+    --return text
+  end
 end
- 
+
 return {
-  description = "Help plugin. Get info from other plugins.  ",
+  description = "Help plugin. Get info from other plugins.  ", 
   usage = {
     "!help: Show list of plugins.",
-    "!help all: Show all commands for every plugin.",
-    "!help [plugin name]: Commands for that plugin.",
-    "!help [number]: Commands for that plugin. Type !help to get the plugin number."
   },
   patterns = {
-    "^!help$",
-    "^!help all",
-    "^!help (.+)"
-  },
-  run = run
+    "^/(help)$",
+  }, 
+  run = run,
 }
- 
-end
